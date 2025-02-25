93942be786256a404c200002a81f65ee5cd8e51e
CedricSoti <152269810+CedricSoti@users.noreply.github.com>
2025-02-25 14:51:39 +0100

    update

--- m133-tag2-anmeldung/control/anmeldung.php
+++ m133-tag2-anmeldung/control/anmeldung.php
@@ -17,21 +17,9 @@ if (( strlen($benutzer) > 0 ) AND ( strlen($password) > 0 ))  {
     $_SESSION['benutzer'] = $benutzer;
     $_SESSION['password'] = $password;
       
-    $html_Output = "<html><head>";
-	$html_Output .= "<title>Anmeldung</title>";
-	$html_Output .= "<style>*{font-size: 28pt;}</style>";
-	$html_Output .= "</head>";
-	$html_Output .= "<body>";
-	$html_Output .= "Hallo, <strong>".$_SESSION["benutzer"]."</strong> die Anmeldung war erfolgreich.<br><br>";
-	$html_Output .= "Ich muss aber gestehen,<br>";
-	$html_Output .= "das Passwort wurde gar noch nicht <br>";
-	$html_Output .= "auf Richtigkeit gepr&uuml;ft.<br>";
-	$html_Output .= "Aber zum zeigen wie es geht,<br>";
-	$html_Output .= "taugt das schon mal <br><br>";
-	$html_Output .= " - - und hier musst du dann die weitere Webseite - - <br>";
-	$html_Output .= " - - hineinsetzen und zeigen, wie es weiter geht - - <br><br>";
-	$html_Output .= "<a href=../control/abmeldung.php>abmelden</a>";
-	$html_Output .= "</body></html>";
+
+header("Location: http://localhost:8080/workspace/templatehtml.php");
+
 }
 else {
 	$html_Output = "<html><head><title>Anmeldung</title></head>";
---
+++ tryagain.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//DE" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="de">
+<head profile="http://gmpg.org/xfn/11">
+    
+   <!-- wird noch ausgefüllt -->
+    
+</head>
+<body>
+ 
+<div id="wrapper">
+    
+
+   <div id="header">
+    <h1>Login Failed</h1>
+   </div><!-- header -->
+    
+   <div id="main">
+    <h2>Try Again</h2>
+   </div><!-- main -->
+    
+   <div id="sidebar">
+    <h4>sidebar</h4>
+   </div><!-- sidebar --> 
+    
+   <div id="footer">
+    <h3>footer</h3>
+   </div><!-- footer -->
+    
+</div><!-- wrapper -->
+    
+</body>
+</html>
