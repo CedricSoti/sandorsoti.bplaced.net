fd640e83a595db332b9657dcd9ff536d57aa9a3f
CedricSoti <152269810+CedricSoti@users.noreply.github.com>
2025-02-26 09:11:47 +0100

    Update templatehtml.php

--- workspace/templatehtml.php
+++ workspace/templatehtml.php
@@ -1,39 +1,3 @@
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//DE" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="de">
-<head profile="http://gmpg.org/xfn/11">
-    
-   <!-- wird noch ausgefüllt -->
-    
-</head>
-<body>
-
-<div id="wrapper">
-    
-
-   <div id="header">
-    <h1>Header</h1>
-   </div><!-- header -->
-    
-   <div id="main">
-    <h2>Main</h2>
-   </div><!-- main -->
-    
-   <div id="sidebar">
-    <h4>sidebar</h4>
-   </div><!-- sidebar --> 
-    
-   <div id="footer">
-      <h3>Log out</h3>
-      <a href=../m133-tag2-anmeldung/control/abmeldung.php>abmelden</a>
-   </div><!-- footer -->
-    
-</div><!-- wrapper -->
-    
-</body>
-</html>
-
-
 <!DOCTYPE html>
 <html>
 <head>
